7c0e0c8a8d2eb6e9772bd655f58a74c279db43db
mfgnik <31517000+mfgnik@users.noreply.github.com>
2018-03-19 17:01:35 +0300

    Update image_resize.py

--- image_resize.py
+++ image_resize.py
@@ -39,25 +39,34 @@ def get_image(input_file_path):
     return Image.open(input_file_path)
 
 
+def get_new_size_by_dimension(image, height, width):
+    if height and width:
+        if width / image.size[0] != height / image.size[1]:
+            print('Warning: change of scale')
+        return height, width
+    elif height:
+        scale = height / image.size[1]
+        return image.size[0] * scale, height
+    elif arguments.width:
+        scale = width / image.size[0]
+        return width, image.size[1] * scale
+
+
 def get_new_size(image, arguments):
     if arguments.scale and (arguments.width or arguments.height):
         print('You can not set both size and scale')
-        sys.exit()
+        return None
     elif arguments.scale:
         width, height = map(lambda x: x * arguments.scale, image.size)
-    elif arguments.height and arguments.width:
-        width, height = arguments.width, arguments.height
-        if width / image.size[0] != height / image.size[1]:
-            print('Warning: change of scale')
-    elif arguments.height:
-        scale = arguments.height / image.size[1]
-        width, height = image.size[0] * scale, arguments.height
-    elif arguments.width:
-        scale = arguments.width / image.size[0]
-        width, height = arguments.width, image.size[1] * scale
+    elif arguments.height or arguments.width:
+        width, height = get_new_size_by_dimension(
+            image,
+            arguments.height,
+            arguments.width
+        )
     else:
         print('You did not write enough arguments')
-        sys.exit()
+        return None
     return map(int, (width, height))
 
 
@@ -71,9 +80,10 @@ def get_old_name(file_path):
 
 def get_new_name(file_path, width, heigth):
     old_name = get_old_name(file_path)
-    name = old_name[:old_name.rfind('.')]
+    name_of_image = old_name[:old_name.rfind('.')]
     extension = old_name[old_name.rfind('.'):]
-    new_name = name + '__' + str(width) + 'x' + str(height) + extension
+    size = str(width) + 'x' + str(height)
+    new_name = name_of_image + '__' + size + extension
     return new_name
 
 
@@ -84,7 +94,10 @@ def output_image(image, output_file_path):
 if __name__ == '__main__':
     arguments = parse_arguments()
     image = get_image(arguments.input_path)
-    width, height = get_new_size(image, arguments)
+    if get_new_size(image, arguments) is None:
+        sys.exit()
+    else:
+        width, height = get_new_size(image, arguments)
     new_name = get_new_name(arguments.input_path, width, height)
     output_path = arguments.output_path + new_name
     output_image(resize_image(image, width, height), output_path)

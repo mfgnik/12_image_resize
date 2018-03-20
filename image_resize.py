7b76cb3268cb0357b7ec48595588831dcc65b63a
mfgnik <31517000+mfgnik@users.noreply.github.com>
2018-03-20 23:52:43 +0300

    Update image_resize.py

--- image_resize.py
+++ image_resize.py
@@ -41,33 +41,28 @@ def get_image(input_file_path):
 
 
 def get_new_size_by_dimension(image_width, image_height, height, width):
-    warning = None
     if height and width:
-        allowed_error = 10 ** -6
-        if abs(width / image_width - height / image_height) < allowed_error:
-            warning = 'Warning: change of scale'
-        return height, width, warning
+        return height, width
     elif height:
         aspect_ratio = height / image_height
-        return int(image_width * aspect_ratio), height, warning
+        return int(image_width * aspect_ratio), height
     elif width:
         aspect_ratio = width / image_width
-        return width, int(image_height * aspect_ratio), warning
+        return width, int(image_height * aspect_ratio)
 
 
 def get_new_size(image, arguments):
     if arguments.scale:
         width, height = map(lambda x: int(x * arguments.scale), image.size)
-        warning = None
     else:
         image_width, image_height = image.size
-        width, height, warning = get_new_size_by_dimension(
+        width, height = get_new_size_by_dimension(
             image_width,
             image_height,
             arguments.height,
             arguments.width
         )
-    return width, height, warning
+    return width, height
 
 
 def resize_image(image, width, height):
@@ -93,9 +88,12 @@ if __name__ == '__main__':
     elif not (arguments.height or arguments.width or arguments.scale):
         sys.exit('You did not write enough arguments')
     else:
-        width, height, warning = get_new_size(image, arguments)
-        if warning:
-            print(warning)
+        width, height = get_new_size(image, arguments)
+        if arguments.width and arguments.height:
+            allowed_error = 10 ** -6
+            change_dimension = abs(width / image.width - height / image.height)
+            if change_dimension > allowed_error:
+                print('Warning: change of dimension')
     new_name = get_new_name(arguments.input_path, width, height)
     output_path = arguments.output_path + new_name
     output_image(resize_image(image, width, height), output_path)
